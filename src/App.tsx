e29bd545d426ea6593dc89a421ff1526b5fc8b6e
Simon Weller <s.meierbeck@gmail.com>
2020-05-11 11:13:36 +0200

    Extract DrumKey component

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,32 @@ import React from "react";
 const App = () => (
   <div id="drum-machine">
     <div id="keys">
-      <button id="boom-key" className="drum-pad" data-sound="Boom">
-        Q <audio id="Q" src="/public/sounds/boom.wav" className="clip" />
-      </button>
-      <button id="clap-key" className="drum-pad" data-sound="Clap">
-        W <audio id="W" src="/public/sounds/clap.wav" className="clip" />
-      </button>
-      <button id="hihat-key" className="drum-pad" data-sound="Hi-hat">
-        E <audio id="E" src="/public/sounds/hihat.wav" className="clip" />
-      </button>
-      <button id="kick-key" className="drum-pad" data-sound="Kick">
-        A <audio id="A" src="/public/sounds/kick.wav" className="clip" />
-      </button>
-      <button id="openhat-key" className="drum-pad" data-sound="Open hat">
-        S <audio id="S" src="/public/sounds/openhat.wav" className="clip" />
-      </button>
-      <button id="ride-key" className="drum-pad" data-sound="Ride">
-        D <audio id="D" src="/public/sounds/ride.wav" className="clip" />
-      </button>
-      <button id="snare-key" className="drum-pad" data-sound="Snare">
-        Z <audio id="Z" src="/public/sounds/snare.wav" className="clip" />
-      </button>
-      <button id="tink-key" className="drum-pad" data-sound="Tink">
-        X <audio id="X" src="/public/sounds/tink.wav" className="clip" />
-      </button>
-      <button id="tom-key" className="drum-pad" data-sound="Tom">
-        C <audio id="C" src="/public/sounds/tom.wav" className="clip" />
-      </button>
+      <DrumKey id="boom" letter="Q" name="Boom" />
+      <DrumKey id="clap" letter="W" name="Clap" />
+      <DrumKey id="hihat" letter="E" name="Hi-hat" />
+      <DrumKey id="kick" letter="A" name="Kick" />
+      <DrumKey id="openhat" letter="S" name="Open hat" />
+      <DrumKey id="ride" letter="D" name="Ride" />
+      <DrumKey id="snare" letter="Z" name="Snare" />
+      <DrumKey id="tink" letter="X" name="Tink" />
+      <DrumKey id="tom" letter="C" name="Tom" />
     </div>
 
     <div id="display" />
   </div>
+);
+
+type DrumKeyProps = {
+  id: string;
+  name: string;
+  letter: string;
+};
+
+const DrumKey = ({ id, name, letter }: DrumKeyProps) => (
+  <button id={id} className="drum-pad" data-sound={name}>
+    {letter}
+    <audio id={letter} src={`/public/sounds/${id}.wav`} className="clip" />
+  </button>
 );
 
 export default App;
