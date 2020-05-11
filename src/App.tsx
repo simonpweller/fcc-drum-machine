952e446a58c7efd94284e193878efa8d15229e04
Simon Weller <s.meierbeck@gmail.com>
2020-05-11 12:14:54 +0200

    Update display

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,57 @@
-import React, { useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
-const App = () => (
-  <div id="drum-machine">
-    <div id="keys">
-      <DrumKey id="boom" letter="Q" name="Boom" />
-      <DrumKey id="clap" letter="W" name="Clap" />
-      <DrumKey id="hihat" letter="E" name="Hi-hat" />
-      <DrumKey id="kick" letter="A" name="Kick" />
-      <DrumKey id="openhat" letter="S" name="Open hat" />
-      <DrumKey id="ride" letter="D" name="Ride" />
-      <DrumKey id="snare" letter="Z" name="Snare" />
-      <DrumKey id="tink" letter="X" name="Tink" />
-      <DrumKey id="tom" letter="C" name="Tom" />
+const App = () => {
+  const [display, setDisplay] = useState<string>("");
+
+  return (
+    <div id="drum-machine">
+      <div id="keys">
+        <DrumKey id="boom" letter="Q" name="Boom" setDisplay={setDisplay} />
+        <DrumKey id="clap" letter="W" name="Clap" setDisplay={setDisplay} />
+        <DrumKey id="hihat" letter="E" name="Hi-hat" setDisplay={setDisplay} />
+        <DrumKey id="kick" letter="A" name="Kick" setDisplay={setDisplay} />
+        <DrumKey id="openhat" letter="S" name="Open" setDisplay={setDisplay} />
+        <DrumKey id="ride" letter="D" name="Ride" setDisplay={setDisplay} />
+        <DrumKey id="snare" letter="Z" name="Snare" setDisplay={setDisplay} />
+        <DrumKey id="tink" letter="X" name="Tink" setDisplay={setDisplay} />
+        <DrumKey id="tom" letter="C" name="Tom" setDisplay={setDisplay} />
+      </div>
+
+      <div id="display">{display}</div>
     </div>
-
-    <div id="display" />
-  </div>
-);
+  );
+};
 
 type DrumKeyProps = {
   id: string;
   name: string;
   letter: string;
+  setDisplay: (name: string) => void;
 };
 
-const DrumKey = ({ id, name, letter }: DrumKeyProps) => {
+const DrumKey = ({ id, name, letter, setDisplay }: DrumKeyProps) => {
   const audio = useRef<HTMLAudioElement>(null);
+
+  const handleKeyPush = useCallback(() => {
+    audio.current?.play().catch(() => {});
+    setDisplay(name);
+  }, [audio, setDisplay, name]);
 
   useEffect(() => {
     const playAudioIfKeyMatchesLetter = (e: KeyboardEvent) =>
-      e.key.toUpperCase() === letter && audio.current?.play();
+      e.key.toUpperCase() === letter && handleKeyPush();
 
     document.addEventListener("keypress", playAudioIfKeyMatchesLetter);
     return () =>
       document.removeEventListener("keypress", playAudioIfKeyMatchesLetter);
-  }, [audio, letter]);
+  }, [letter, handleKeyPush]);
 
   return (
     <button
       id={id}
       className="drum-pad"
       data-sound={name}
-      onClick={() => audio.current?.play()}
+      onClick={handleKeyPush}
     >
       {letter}
       <audio
